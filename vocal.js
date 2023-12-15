8632c2f16e151b3ba9f4c65d72aedfe133827cb5
Fabian Granados <av7811923@gmail.com>
2023-12-15 15:45:45 -0500

    vocales

---
+++ vocal.js
@@ -0,0 +1,3 @@
+let letra=prompt("Ingrese la letra en minuscula:");
+const vocal=String(letra) == "a" || String(letra) == "e" || String(letra) == "i" || String(letra) == "o" || String(letra) == "u";
+console.log("¿Su letra es vocal?: " + vocal);
